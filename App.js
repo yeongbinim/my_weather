b24fa15b468c495d4d1783796e375fb386d99cf1
yeong bin <2976622@naver.com>
2021-06-24 19:06:18 +0900

    my_weather

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+import {Alert} from 'react-native';
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from './Weather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "4d4d60726490b7f6cc61139e0a3328f0";
+
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  };
+  getWeather = async(latitude, longitude) =>{
+    const {
+      data: {
+        main: {temp},
+        weather
+      }
+    } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+
+    this.setState({
+      isLoading:false, 
+      temp, 
+      condition:weather[0].main
+    });
+  }
+  getLocation = async() =>{
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const { coords : { latitude, longitude}} =  await Location.getCurrentPositionAsync();
+      this.getWeather(latitude,longitude);
+    }
+    catch (error){
+      
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const { isLoading, temp, condition} = this.state;
+    return <Loading/>;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,17 @@
+# [Nomadcoders]my_weather App
+
+ Nomadcoders 수업에서 ReactNative로 클론코딩한 앱 입니다.
+
+---
+
+### 결과물
+
+<img src="./result.gif" style="float:left; width:30%;"/>
+
+---
+
+### 요약
+
+1. expo-location 모듈내의 함수를 통해 **사용자의 위치**를 불러옵니다.
+2. axios함수를 통해 https://openweathermap.org/의 api로 **사용자 위치**에 해당하는 **날씨 데이터**를 요청합니다.
+3. **날씨 데이터**에 따른 아이콘, 온도, 배경, 문구를 보여줍니다.
